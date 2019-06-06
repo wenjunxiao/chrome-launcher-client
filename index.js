b14333c815dc7d491cdfb3b6a974788024ca5bc5
Wenjun Xiao <wenjunxiao@126.com>
2019-06-06 14:26:44 +0800

    add error and query

--- index.js
+++ index.js
@@ -3,16 +3,25 @@
 const request = require('superagent');
 const uuid = require('uuid');
 
-function launch(opts, host) {
+function launch(opts, host, query) {
   const uid = uuid();
   return request
     .post(`http://${host}:${opts.port}/api/launch/${uid}`)
     .timeout(opts.launchTimeout || 10 * 60 * 1000)
+    .query(Object.assign({_: Date.now()}, query))
     .send(opts).then(rsp => {
-      const data = rsp.body.data || {};
+      const data = rsp.body && rsp.body.data;
+      if (!data) {
+        const error = Object.assign(new Error(''), rsp.body && rsp.body.error || {
+          message: rsp.text
+        })
+        error.message = 'Remote chrome launch failed:' + error.message;
+        return Promise.reject(error);
+      }
       return Object.assign({ pid: uid }, data, {
         kill() {
           return request.post(`http://${host}:${opts.port}/api/kill/${uid}`)
+            .query(Object.assign({_: Date.now()}, query))
             .send(data)
             .then(rsp => {
               return rsp.body.success
